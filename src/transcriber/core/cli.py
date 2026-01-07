2335a6872c8b4c35d844938412b394319d4fbfed
Will Barmby <willbarmby@gmail.com>
2026-01-07 13:44:38 -0500

    Introduce subcommand-based CLI for file, batch, and watch modes

--- src/transcriber/core/cli.py
+++ src/transcriber/core/cli.py
@@ -1,9 +1,45 @@
 import argparse
+from pathlib import Path
+
+
+def directory(value: str) -> Path:
+    try:
+        path_value = Path(value)
+    except ValueError:
+        raise argparse.ArgumentTypeError(f"{value} is not a valid path")
+    if path_value.is_dir():
+        return path_value
+    else:
+        raise argparse.ArgumentTypeError(f"{value} is not a valid directory")
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
+
     parser.add_argument(
         "--verbose", "-v", help="displays debugging log entries", action="store_true"
     )
+
+    subparser = parser.add_subparsers(dest="command", required=True)
+
+    file_parser = subparser.add_parser("file")
+    file_parser.add_argument(
+        "path", type=Path, help="the path to the audio file to transcribe"
+    )
+
+    batch_parser = subparser.add_parser("batch")
+    batch_parser.add_argument(
+        "directory",
+        type=directory,
+        help="the directory containing the files to transcribe",
+    )
+
+    watch_parser = subparser.add_parser("watch")
+    watch_parser.add_argument(
+        "directory",
+        type=directory,
+        help="the directory to watch for files to transcribe (default: ~/Downloads)",
+        default=Path.home() / "Downloads",
+    )
+
     return parser.parse_args()
--- src/transcriber/main.py
+++ src/transcriber/main.py
@@ -1,18 +1,35 @@
 import logging
 import time
+import argparse
 from transcriber.core.logging import set_up_logging
 from transcriber.core.cli import parse_args
 from transcriber.pipelines.observers import setup_observers
 from transcriber.config.paths import TEXT_DIR, ARCHIVE_DIR, FINAL_DIR
 
 
-def main(verbose: bool | None = None):
+def main():
     args = parse_args()
-    if verbose is None:
-        verbose = args.verbose
 
-    level = logging.DEBUG if verbose else logging.INFO
+    level = logging.DEBUG if args.verbose else logging.INFO
     set_up_logging(level=level)
+
+    if args.command == "file":
+        run_file_mode(args)
+    if args.command == "batch":
+        run_batch_mode(args)
+    if args.command == "watch":
+        run_watch_mode(args)
+
+
+def run_file_mode(args: argparse.Namespace):
+    return
+
+
+def run_batch_mode(args: argparse.Namespace):
+    return
+
+
+def run_watch_mode(args: argparse.Namespace):
 
     for path in [FINAL_DIR, ARCHIVE_DIR, TEXT_DIR]:
         path.mkdir(parents=True, exist_ok=True)
